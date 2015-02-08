4a1fe6c9b443a8ffe00f03240ecb163eb0bdfa96
Akihiro Motoki <motoki@da.jp.nec.com>
2015-02-09 05:08:58 +0900

    Add devstack plugin for networking-nec

---
+++ devstack/plugin.sh
@@ -0,0 +1,9 @@
+NETWORKING_NEC_DIR=$(cd $(dirname ${BASH_SOURCE}/..) && pwd)
+
+function install_networking_nec {
+    setup_develop $NETWORKING_NEC_DIR
+}
+
+if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+    install_networking_nec
+fi
