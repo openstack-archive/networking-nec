e9191f2e803db4b7b3278f3df3ff29a197597842
Akihiro Motoki <motoki@da.jp.nec.com>
2015-03-08 16:00:27 +0900

    Move plugin-specific devstack code to its own repo
    By using devstack external plugin mechanism, we no longer
    need to have plugin configuration in the upstream devstack.
    
    Change-Id: I005a0657689514ae8906a3c4ac9c57dd23393b5f

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,9 +1,17 @@
 NETWORKING_NEC_DIR=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
 
-function install_networking_nec {
-    setup_develop $NETWORKING_NEC_DIR
+function source_plugin {
+    if [[ "$Q_PLUGIN" == "nec" ]]; then
+        source $NETWORKING_NEC_DIR/devstack/lib/nec_plugin
+    fi
 }
 
 if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
-    install_networking_nec
+    source_plugin
+elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+    setup_develop $NETWORKING_NEC_DIR
+elif [[ "$1" == "unstack" ]]; then
+    source_plugin
+elif [[ "$1" == "clean" ]]; then
+    source_plugin
 fi
