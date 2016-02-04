2109bc8f644c7f5e609aaa0d582550894adcda80
Akihiro Motoki <motoki@da.jp.nec.com>
2016-02-04 21:32:35 +0900

    Drop OpenFlow plugin
    It was marked as deprecated in Liberty release.
    
    DevStack plugin and top-level README will be updated
    after a series of NWA plugin patches are merged.
    
    Change-Id: I4c5cced58c138e6a01b61788fdbbc147f635d778

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -2,5 +2,12 @@
 
 TESTRARGS=$1
 
+# Delete bytecodes from normal directories before running tests.
+# Note that bytecodes in dot directories will not be deleted
+# to keep bytecodes of python modules installed into virtualenvs.
+sh -c "find . -type d -name '.?*' -prune -o \
+    \( -type d -name '__pycache__' -o -type f -name '*.py[co]' \) \
+    -print0 | xargs -0 rm -rf"
+
 exec 3>&1
 status=$(exec 4>&1 >&3; ( python setup.py testr --slowest --testr-args="--subunit $TESTRARGS"; echo $? >&4 ) | subunit-trace -f) && exit $status
