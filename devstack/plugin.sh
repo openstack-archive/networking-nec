718ce3e6bf36513e597e6eb768307ededbf41b1d
Akihiro Motoki <motoki@da.jp.nec.com>
2016-02-14 16:23:24 +0900

    DevStack plugin support for NWA plugin
    OpenFlow plugin code has been dropped already.
    This commit switches devstack plugin to support NWA plugin.
    
    Remove a bunch of OpenFlow plugin specific functions.
    
    Change-Id: I861eb67d7c23033bd7c38de550c5feff15b0fc8d

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -10,6 +10,13 @@ if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
     source_plugin
 elif [[ "$1" == "stack" && "$2" == "install" ]]; then
     setup_develop $NETWORKING_NEC_DIR
+elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+    # It needs to be started before starting neutron-server.
+    # run_phase extra is too late because create_neutron_initial_network
+    # will be called just after start_neutron_service_and_check.
+    start_nwa_agent
+elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+    :
 elif [[ "$1" == "unstack" ]]; then
     source_plugin
 elif [[ "$1" == "clean" ]]; then
