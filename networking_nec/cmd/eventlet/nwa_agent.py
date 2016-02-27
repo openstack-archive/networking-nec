16adfb6658ef52681a90313a6a30cdb4544733a7
Akihiro Motoki <motoki@da.jp.nec.com>
2016-02-28 07:46:22 +0900

    Define main() in networking_nec.cmd.eventlet.nwa_agent
    It is required to allow nwa-agent to run through console_scripts
    
    Change-Id: I29f0b46193a6ec9348ff120fd3ec9242d301dd33
    Closes-Bug: #1550809

--- networking_nec/cmd/eventlet/nwa_agent.py
+++ networking_nec/cmd/eventlet/nwa_agent.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 # PBR Generated from u'console_scripts'
 
-import sys
-
 from networking_nec.nwa.agent import nwa_agent
 
 
-if __name__ == "__main__":
-    sys.exit(nwa_agent.main())
+def main():
+    nwa_agent.main()
