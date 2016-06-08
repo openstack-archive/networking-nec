8859ef1b58379b5b2899415d6722b95967dfc8f5
Akihiro Motoki <motoki@da.jp.nec.com>
2016-06-08 20:14:12 +0000

    Fix broken devstack plugin
    The backport of NWA plugin patch recently merged broke
    devstack plugin due to the syntax error. Is it tested?
    
    In addition, there is no need to run nec-nwa-agent
    when OpenFlow plugin is used.
    
    Change-Id: I8867ad84510f97a14990bf1ea82662044cd50968

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,7 +2,19 @@ NETWORKING_NEC_DIR=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
 
 function source_plugin {
     if [[ "$Q_PLUGIN" == "nec" ]]; then
-        source $NETWORKING_NEC_DIR/devstack/lib/nec_plugin
+        if is_nwa_plugin; then
+            source $NETWORKING_NEC_DIR/devstack/lib/nwa_plugin
+        else
+            source $NETWORKING_NEC_DIR/devstack/lib/nec_plugin
+        fi
+    fi
+}
+
+function is_nwa_plugin {
+    if [[ -n "$NECNWA_SERVER_URL" ]]; then
+        return 0
+    else
+        return 1
     fi
 }
 
@@ -11,13 +23,15 @@ if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
 elif [[ "$1" == "stack" && "$2" == "install" ]]; then
     setup_develop $NETWORKING_NEC_DIR
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-    # This must be done after neutron database is populated and
-    # before starting neutron server.
-    populate_nwa_database
-    # It needs to be started before starting neutron-server.
-    # run_phase extra is too late because create_neutron_initial_network
-    # will be called just after start_neutron_service_and_check.
-    start_nwa_agent
+    if is_nwa_plugin; then
+        # This must be done after neutron database is populated and
+        # before starting neutron server.
+        populate_nwa_database
+        # It needs to be started before starting neutron-server.
+        # run_phase extra is too late because create_neutron_initial_network
+        # will be called just after start_neutron_service_and_check.
+        start_nwa_agent
+    fi
 elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
     :
 elif [[ "$1" == "unstack" ]]; then
